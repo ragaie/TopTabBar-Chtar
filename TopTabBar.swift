2e6a782e62ed50ed806e1390d21bf822762a4a27
Ragaie alfy Fahmey <eng.ragaie@gmail.com>
2019-11-09 13:03:27 +0200

    remove shake func

--- TopTabBar.swift
+++ TopTabBar.swift
@@ -323,13 +323,13 @@ import UIKit
     }
 }
 
-extension UIView {
-    func shake() {
-        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-        animation.duration = 0.9
-        animation.values = [-15.0, 15.0, -15.0, 15.0, -7.0, 7.0, -2.50, 2.50, 0.0 ]
-        layer.add(animation, forKey: "shake")
-    }
-    
-}
+//extension UIView {
+//    func shake() {
+//        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+//        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+//        animation.duration = 0.9
+//        animation.values = [-15.0, 15.0, -15.0, 15.0, -7.0, 7.0, -2.50, 2.50, 0.0 ]
+//        layer.add(animation, forKey: "shake")
+//    }
+//    
+//}
